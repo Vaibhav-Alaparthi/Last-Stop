e269d5020637791ff9777a5f01cf9662cd308621
Wang <wangyil26@issaquah.wednet.edu>
2026-04-07 11:15:17 -0700

    Slide

---
+++ My project/Assets/Scripts/PlayerSlide.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using System.Collections;
+
+public class PlayerSlide : MonoBehaviour
+{
+    public Animator animator;
+    public CapsuleCollider capsule;
+
+    [Header("Slide Key")]
+    public KeyCode slideKey = KeyCode.LeftControl;
+
+    [Header("Slide Timing")]
+    public float slideDuration = 0.8f;
+
+    [Header("Standing Collider")]
+    public float standingHeight = 2f;
+    public Vector3 standingCenter = new Vector3(0f, 1f, 0f);
+
+    [Header("Sliding Collider")]
+    public float slidingHeight = 1f;
+    public Vector3 slidingCenter = new Vector3(0f, 0.5f, 0f);
+
+    private bool isSliding = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(slideKey) && !isSliding)
+        {
+            StartCoroutine(SlideRoutine());
+        }
+    }
+
+    IEnumerator SlideRoutine()
+    {
+        isSliding = true;
+
+        capsule.height = slidingHeight;
+        capsule.center = slidingCenter;
+
+        animator.SetTrigger("Slide");
+
+        yield return new WaitForSeconds(slideDuration);
+
+        capsule.height = standingHeight;
+        capsule.center = standingCenter;
+
+        isSliding = false;
+    }
+}
