7267a5863a6502ec69f6ff8d544caa35555574dc
Wang <wangyil26@issaquah.wednet.edu>
2026-04-07 10:11:07 -0700

    Animations

--- My project/Assets/Scripts/PlayerMovement.cs
+++ My project/Assets/Scripts/PlayerMovement.cs
@@ -4,34 +4,63 @@ public class PlayerMovement : MonoBehaviour
 {
     public float sideSpeed = 6f;
     public float forwardSpeed = 8f;
-    public float jumpForce = 2.5f;
+    public float jumpForce = 5f;
 
     private Rigidbody rb;
-    private bool isGrounded;
+    private Animator animator;
+    private bool isGrounded = false;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
 
-        // stop tipping over
         rb.freezeRotation = true;
+
+        if (animator != null)
+        {
+            animator.SetBool("IsGrounded", true);
+        }
     }
 
     void Update()
     {
-        float moveX = Input.GetAxis("Horizontal");
+        float moveX = Input.GetAxisRaw("Horizontal");
 
-        rb.linearVelocity = new Vector3(
-            moveX * sideSpeed,
-            rb.linearVelocity.y,
-            forwardSpeed
-        );
+        Vector3 velocity = rb.linearVelocity;
+        velocity.x = moveX * sideSpeed;
+        velocity.z = forwardSpeed;
+        rb.linearVelocity = velocity;
 
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
-            rb.linearVelocity = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
-            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isGrounded = false;
+
+            if (animator != null)
+            {
+                animator.SetBool("IsGrounded", false);
+                animator.ResetTrigger("Jump");
+                animator.SetTrigger("Jump");
+            }
+
+            velocity = rb.linearVelocity;
+            velocity.y = 0f;
+            rb.linearVelocity = velocity;
+
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+
+            if (animator != null)
+            {
+                animator.SetBool("IsGrounded", true);
+            }
         }
     }
 
@@ -40,6 +69,11 @@ public class PlayerMovement : MonoBehaviour
         if (collision.gameObject.CompareTag("Ground"))
         {
             isGrounded = true;
+
+            if (animator != null)
+            {
+                animator.SetBool("IsGrounded", true);
+            }
         }
     }
 
@@ -48,6 +82,11 @@ public class PlayerMovement : MonoBehaviour
         if (collision.gameObject.CompareTag("Ground"))
         {
             isGrounded = false;
+
+            if (animator != null)
+            {
+                animator.SetBool("IsGrounded", false);
+            }
         }
     }
 }
